3dc03a8ef0a61197b9364bd8f734a5248fa0a5c0
Nils Plaschke <plaschke@adobe.com>
2018-12-14 11:05:16 +0100

    Trying to fix the shell script

--- hooks/setup-travis.sh
+++ hooks/setup-travis.sh
@@ -3,24 +3,11 @@
 set -o errexit
 
 main() {
-  update_docker
   install_sledgehammer
 
   echo "INFO:
   Done! Finished setting up Travis-CI machine.
   "
-}
-
-# Updated docker to the latest version
-update_docker() {
-  echo "
-  INFO:  Updating docker...
-"
-
-  sudo apt update -y
-  sudo apt install --only-upgrade docker-ce -y
-
-  docker info
 }
 
 # Installs Sledgehammer to have access to build tools
